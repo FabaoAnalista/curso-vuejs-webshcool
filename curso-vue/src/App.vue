9e37da2fadd2c2989be5c87c4d9e797b236c63dd
fabaoufop <fabaoanalista@gmail.com>
2018-03-23 16:23:42 -0300

    primeiro component

--- curso-vue/src/App.vue
+++ curso-vue/src/App.vue
@@ -1,28 +1,21 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld/>
-  </div>
+<div>
+  <cc-hello></cc-hello>
+</div>
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld'
+import CcHello from './components/Hello.vue'
 
 export default {
-  name: 'App',
+  name: 'WebscholComponent',
   components: {
-    HelloWorld
+    CcHello
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
